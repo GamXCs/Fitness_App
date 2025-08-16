01df0eb9f78065121cb69a2fa98dbfc1c1b730dd
Walter Stewart <gavlevi@uab.edu>
2025-08-16 15:20:16 -0500

    Initial Commit

---
+++ fitness_app/fitness_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitness_appApp.swift
+//  fitness_app
+//
+//  Created by Gamliel Israel on 8/16/25.
+//
+
+import SwiftUI
+
+@main
+struct fitness_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fitness_appTests/fitness_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  fitness_appTests.swift
+//  fitness_appTests
+//
+//  Created by Gamliel Israel on 8/16/25.
+//
+
+import Testing
+@testable import fitness_app
+
+struct fitness_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
